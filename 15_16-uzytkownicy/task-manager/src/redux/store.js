8339a016c3284a1b5ad1653fa8059318faac3730
jacekpietrzak <jacekpietrzak@me.com>
2023-02-28 13:48:47 +0100

    Lesson 15 finished

---
+++ 15_16-uzytkownicy/task-manager/src/redux/store.js
@@ -0,0 +1,46 @@
+import { configureStore, getDefaultMiddleware } from "@reduxjs/toolkit";
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from "redux-persist";
+import storage from "redux-persist/lib/storage";
+import { tasksReducer } from "./tasks/slice";
+import { authReducer } from "./auth/slice";
+
+const middleware = [
+  ...getDefaultMiddleware({
+    serializableCheck: {
+      ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+    },
+  }),
+];
+
+// Persisting token field from auth slice to localstorage
+const authPersistConfig = {
+  key: "auth",
+  storage,
+  whitelist: ["token"],
+};
+
+const tasksPersistConfig = {
+  key: "tasks",
+  storage,
+};
+
+export const store = configureStore({
+  reducer: {
+    auth: persistReducer(authPersistConfig, authReducer),
+    // tasks: tasksReducer,
+    tasks: persistReducer(tasksPersistConfig, tasksReducer),
+  },
+  middleware,
+  devTools: process.env.NODE_ENV === "development",
+});
+
+export const persistor = persistStore(store);
